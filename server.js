5bf36b104a5cead5fca2d7ad5b836be8f843ca8d
kuttimathan <30966163+kuttimathan@users.noreply.github.com>
2017-08-13 16:12:30 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -80,6 +80,14 @@ app.get('/counter', function (req, res) {
     res.send(counter.toString());
 });
 
+var names = [];
+app.get('/submit-name/name', function (req, res) {
+    var name = req.params.name;
+    name.push(name);
+    res.send(names);
+    res.send(counter.toString());
+});
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
