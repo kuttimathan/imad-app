7d792a6da93bb1b1e7ba259126e04cb26fbadd4d
kuttimathan <30966163+kuttimathan@users.noreply.github.com>
2017-08-13 11:44:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,12 +5,60 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var contentContact = {
+  title: 'Contact Us',
+  heading: 'Address',
+  address: 'Chennai, Tamilnadu',
+  content: `
+    <p>
+        Please contact us at codebulls@gmail.com
+    </p>
+  `
+};
+function createTemplate (data) {
+
+    var title = data.title;
+    var heading = data.heading;
+    var address = data.address;
+    var content = data.content;
+    var htmlTemplate = `
+    <html>
+        <head>
+            <title>
+                ${title}
+            </title>
+            <meta name="viewport" content="width=device-width, initial-scale=1"/>
+            <link href="/ui/style.css" rel="stylesheet" />
+        </head>
+        <body>
+            <div class="container"> 
+                <hr/>
+                <div>
+                    <a href="/">Home</a>
+                </div>
+                <hr/>
+                <h3>
+                    ${heading}
+                </h3>
+                <div>
+                    ${address}
+                </div>
+                <div>
+                    ${content}
+                </div>
+            </div>
+        </body>
+    </html>
+    `;
+    return htmlTemplate
+}
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
 app.get('/contact', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'contact.html'));
+  res.send(createTemplate(contentContact));
 });
 
 app.get('/products', function (req, res) {
@@ -34,6 +82,8 @@ app.get('/ui/madi.png', function (req, res) {
 // Do not change port, otherwise your app won't run on IMAD servers
 // Use 8080 only for local development if you already have apache running on 80
 //  res.send("Products Information Requested"); to send a text response rather than a file
+//  res.sendFile(path.join(__dirname, 'ui', 'contact.html')); to send a file response
+
 
 var port = 80;
 app.listen(port, function () {
