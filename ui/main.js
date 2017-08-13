5a244393e2f06899d88f861674a58e765af542f5
kuttimathan <30966163+kuttimathan@users.noreply.github.com>
2017-08-13 14:11:44 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,6 +4,11 @@ var element = document.getElementById('main-text')
 element.innerHTML = 'CodeBulls Inc.'
 //Move the Image
 var img = document.getElementById('main-image')
+function moveRight(){
+    marginLeft = marginLeft + 10;
+    img.style.marginRight = marginLeft + 'px';
+}
 img.onclick = function(){
-    img.style.marginLeft='200px';
+    var interval = setInterval(moveRight, 100);
+    //img.style.marginRight='200px'; One time move
 }
