bc0f4ec0caf5c8b8b464b5f6abbfaa867ec7e9b8
kuttimathan <30966163+kuttimathan@users.noreply.github.com>
2017-08-13 14:02:07 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,3 +2,8 @@ console.log('Loaded!');
 //Change a field name
 var element = document.getElementById('main-text')
 element.innerHTML = 'CodeBulls Inc.'
+//Move the Image
+var img = document.getElementById('main-image')
+img.onclick = function(){
+    img.style.marginLeft="180px";
+}
