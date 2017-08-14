596e95eeb49b24ffb35ffa505cfa33007f66da93
kuttimathan <30966163+kuttimathan@users.noreply.github.com>
2017-08-14 09:16:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+    user: 'kuttimathan',
+    database: 'kuttimathan',
+    host: 'db.imad.hasura-app.io',
+    port: '5432',
+    password: process.env.DB_PASSWORD
+};
 
 var app = express();
+
 app.use(morgan('combined'));
 
 var contents = {
@@ -89,6 +99,20 @@ app.get('/submit-name', function (req, res) {
     //JSON Javascript Object Notation - converting js scripts to string
     res.send(JSON.stringify(names));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function (req, res) {
+//make a select request
+//return response
+pool.query("SELECT * FROM category", function (err, result){
+    if (err) {
+        res.status(500).send(err.toString());
+    } else {
+        res.send(JSON.stringify(result));
+    }
+})
+});
+
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
