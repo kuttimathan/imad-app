1cca3015135f04db71d72836a0c216e93dc83f99
kuttimathan <30966163+kuttimathan@users.noreply.github.com>
2017-08-14 11:16:45 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -126,8 +126,7 @@ app.get('/', function (req, res) {
 app.get('/contents/:contentName', function (req, res) {
     pool.query('SELECT * FROM appdata WHERE title = ' + req.params.contentName , function(err, result) {
        if(err) {
-           alert('amhit');
-           res.status(500).send(err.toString());
+           res.status(500).send('check' + err.toString());
        } else {
            if (result.rows.length === 0) {
                res.status(404).send("Data not found");
